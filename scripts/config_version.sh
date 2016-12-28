716e2d163072364ae69ebe7bbd989db339012911
Nick Maludy <nick.maludy@encore.tech>
2016-12-28 18:17:11 -0500

    Attempt to fix config_version error on puppet 3.6

--- scripts/config_version.sh
+++ scripts/config_version.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-if [ -e $1/$2/.r10k-deploy.json ]
-then
-  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/code_manager_config_version.rb $1 $2
-elif [ -e /opt/puppetlabs/server/pe_version ]
-then 
-  /opt/puppetlabs/puppet/bin/ruby $1/$2/scripts/config_version.rb $1 $2  
+
+local ruby_exe='ruby'
+if [ -e /opt/puppetlabs/puppet/bin/ruby]; then
+  ruby_exe='/opt/puppetlabs/puppet/bin/ruby'
+fi
+
+if [ -e $1/$2/.r10k-deploy.json ]; then
+  $ruby_exe $1/$2/scripts/code_manager_config_version.rb $1 $2
+elif [ -e /opt/puppetlabs/server/pe_version ]; then 
+  $ruby_exe $1/$2/scripts/config_version.rb $1 $2  
 else
   /usr/bin/git --version > /dev/null 2>&1 &&
   /usr/bin/git --git-dir $1/$2/.git rev-parse HEAD ||
