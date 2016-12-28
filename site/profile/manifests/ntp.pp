734e60eee54c261f823b633e619810d9f6eb6667
Nick Maludy <nick.maludy@encore.tech>
2016-12-28 18:37:27 -0500

    Fixed bug in NTP (missing comma)

--- site/profile/manifests/ntp.pp
+++ site/profile/manifests/ntp.pp
@@ -26,7 +26,7 @@ class profile::ntp {
     # start at boot
     service_enable => true,
     # ensure running currently
-    service_ensure => 'running'
+    service_ensure => 'running',
     # NTP server
     servers => ["0.pool.ntp.org", "1.pool.ntp.org", "2.pool.ntp.org"],
   }
