24219f54ca4d4e519bf9ce7a65f22f7924a0d238
Nick Maludy <nick.maludy@encore.tech>
2017-01-02 17:41:51 -0500

    Added variable to override MOTD in hiera

--- site/profile/manifests/motd.pp
+++ site/profile/manifests/motd.pp
@@ -1,6 +1,13 @@
 class profile::motd {
-  include ::motd
-  # class { '::motd':
-  #   content => "This server, ${::fqdn}, is managed by Puppet\n",
-  # }
+
+  $encore_motd_content = hiera("encore::motd::content", undef)
+
+  if $encore_motd_content {
+    class { '::motd':
+      content => "${encore_motd_content}\n",
+    }
+  }
+  else {
+    include ::motd
+  }
 }
