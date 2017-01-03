b2cd7c2b6ddf107efb21c663d7851575c78bb302
Nick Maludy <nick.maludy@encore.tech>
2017-01-03 15:48:02 -0500

    trying to install mcollective client

--- site/profile/manifests/mcollective/server.pp
+++ site/profile/manifests/mcollective/server.pp
@@ -2,7 +2,6 @@ class profile::mcollective::server {
   $middleware_hosts = hiera("encore::mcollective::middleware_hosts")
   
   class { '::mcollective':
-    client           => true,
     middleware_hosts => $middleware_hosts,
   }
 }
