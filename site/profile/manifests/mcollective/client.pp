f07d191e0f866d72a7725da419d819689062bd0f
Nick Maludy <nick.maludy@encore.tech>
2017-01-04 18:42:03 -0500

    tryin to fix mcollective issue

--- site/profile/manifests/mcollective/client.pp
+++ site/profile/manifests/mcollective/client.pp
@@ -8,5 +8,10 @@ class profile::mcollective::client {
   class { '::mcollective':
     client           => true,
     middleware_hosts => $middleware_hosts,
+    # This is the installed location using the profile::mcollective::repo
+    # we need to override this because the ::mcollective module
+    # specifies the wrong location in: manifests/default.pp
+    # This causes mcollective to fail to start unless changed.
+    core_libdir => '/usr/local/libexec/mcollective'
   }
 }
--- site/profile/manifests/mcollective/server.pp
+++ site/profile/manifests/mcollective/server.pp
@@ -7,5 +7,10 @@ class profile::mcollective::server {
   
   class { '::mcollective':
     middleware_hosts => $middleware_hosts,
+    # This is the installed location using the profile::mcollective::repo
+    # we need to override this because the ::mcollective module
+    # specifies the wrong location in: manifests/default.pp
+    # This causes mcollective to fail to start unless changed.
+    core_libdir => '/usr/local/libexec/mcollective'
   }
 }
