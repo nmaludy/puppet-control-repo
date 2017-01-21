d25b553d1daa107574c935409b247cec9413f078
Nick Maludy <nick.maludy@encore.tech>
2017-01-21 12:40:20 -0500

    Added virt who to base role

--- site/role/manifests/base.pp
+++ site/role/manifests/base.pp
@@ -6,6 +6,7 @@ class role::base {
   include profile::motd
   include profile::ntp
   include profile::snmp
+  include profile::virt_who
   # @note The term "server" in mcollective means that this is a node
   #       that receives mcollective requests and performs actions
   # @link https://docs.puppet.com/mcollective/overview_components.html
